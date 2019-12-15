aa90c1cf8b2efedc3d190944890f0009edbab576
Chris Hoge <chris@hogepodge.com>
2019-12-14 21:28:35 -0800

    Added a README.md and better watcher script

---
+++ README.md
@@ -0,0 +1,3 @@
+# capi
+
+A quick way to set up Cluster-API with the Docker provider.
--- watcher.sh
+++ watcher.sh
@@ -1,5 +1,7 @@
+#!/bin/bash
+set -x
 
-
-
-kubectl --kubeconfig=./capi-quickstart.kubeconfig get nodes
+watch -n 30 "docker ps; \
+	kubectl get pods --all-namespaces; \
+	kubectl --kubeconfig=./capi-quickstart.kubeconfig get nodes"
 
